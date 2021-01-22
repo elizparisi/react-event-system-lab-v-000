088c9520510dd124eb0d5e2d5ecdbddceb6a8efe
Liz P <parisielizabeth@gmail.com>
2021-01-22 22:17:06 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 
 export default class Keypad extends Component {
   
+  handleInputPassword()
+  
   render() {
     return(
       <div> <input type="password" /> </div>
