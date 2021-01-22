b86c20851afc333c9f91e6e8be28f4cc3406c81d
Liz P <parisielizabeth@gmail.com>
2021-01-22 22:23:00 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 
 export default class EyesOnMe extends Component {
   
-  onFocus
+  onFocus = () => {
+    console.log('Good!');
+  }
+  
+  onBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
   
   render() {
     return(
